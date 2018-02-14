a60a443feb570e7e4d0f0283168c03cd2b8119db
Poonam Mhaskar <poonammhaskar@Poonams-MacBook-Pro.local>
2018-02-13 21:04:20 -0800

    create/update account

--- User/customer_profile.php
+++ User/customer_profile.php
@@ -56,18 +56,24 @@
 			$zip = $_POST['zip'];
 		}
 
+        $userid = $_SESSION["userid"];
 		if(!empty($fname)) {
 			//get the userid from user table
 			$query = "SELECT id FROM user where phonenumber=\"".$phone."\"";
 			// Sign In
 			$result = $mysqli->query($query);
-			if ($result->num_rows > 0) {
+
+            $query2 = "SELECT id FROM userdetail where userid=\"".$userid."\"";
+            $result2 = $mysqli->query($query2);
+			// Sign In
+			$result = $mysqli->query($query);
+			if ($result2->num_rows == 0 && $result->num_rows > 0) {
 				$row = $result->fetch_array();
-				$userid = $row["id"];
+				$user_id = $row["id"];
 				//insert the entry in userdetail table
 				// insert into table
 				$query = "INSERT INTO userdetail(userid, firstname, lastname, phonenumber,
-					address1, address2, city, state, country, zipcode, modified, created) VALUES (\"".$userid."\",\"".$fname."\",\"". $lname."\",\"". $phone."\",\"". $street1."\",\"". $street2."\"
+					address1, address2, city, state, country, zipcode, modified, created) VALUES (\"".$user_id."\",\"".$fname."\",\"". $lname."\",\"". $phone."\",\"". $street1."\",\"". $street2."\"
 							,\"". $city."\",\"". $state."\",\"". $country."\",\"". $zip."\", sysdate(), sysdate())";
 				echo $query;
 				$result = $mysqli->query($query);
@@ -77,14 +83,41 @@
 					echo "Failed to update profile";
 				}
 			} else {
-				echo "Invalid username/password";
+                $query = "UPDATE userdetail
+                          SET firstname = \"".$fname."\", lastname = \"".$lname."\",  phonenumber= \"".$phone."\", address1 = \"".$street1."\",
+                             address2 = \"".$street2."\", city = \"".$city."\", state = \"".$state."\", country = \"".$country."\", zipcode = \"".$zip."\",
+                             modified = sysdate()
+                          WHERE userid=".$userid;
+                $result = $mysqli->query($query);
+                if ($result) {
+                    echo '<script>window.location.href = "/Treaty/User/customer.php";</script>';
+                } else {
+                    echo "Failed to update profile";
+                }
 			}
 		}  else {
-			$query = "SELECT id FROM userdetail where userid=\"".$userid."\"";
+
+			$query = "SELECT id,firstname,lastname, phonenumber, address1, address2, city, state, country, zipcode
+					  FROM userdetail
+					  WHERE userid=\"".$userid."\"";
+
 			$result = $mysqli->query($query);
+			$profileresultset = array();
 			// if yes, display dashboard link
 			if ($result->num_rows > 0) {
 				$_SESSION['customerdashboard'] = true;
+				// output data of each row
+                $row = $result->fetch_array();
+                array_push($profileresultset, $row["firstname"]);
+    			array_push($profileresultset, $row["lastname"]);
+    			array_push($profileresultset, $row["phonenumber"]);
+    			array_push($profileresultset, $row["address1"]);
+    			array_push($profileresultset, $row["address2"]);
+                array_push($profileresultset, $row["city"]);
+    			array_push($profileresultset, $row["state"]);
+    			array_push($profileresultset, $row["country"]);
+    			array_push($profileresultset, $row["zipcode"]);
+
 			} else {
 				// donot display dashboard link
 				$_SESSION['customerdashboard'] = false;
@@ -128,8 +161,13 @@
 
 					<div class="tab-left">
 						<ul class="resp-tabs-list">
-							<li class="resp-tab-item-profile"><i class="fa fa-plane" aria-hidden="true"></i>Create Account</li>
-							<li class="resp-tab-item-profile"><i class="fa fa-plane" aria-hidden="true"></i>Update Account</li>
+                            <?php
+                                 if(!isset($profileresultset)) {
+		                              echo "<li class=\"resp-tab-item-profile\"><i class=\"fa fa-plane\" aria-hidden=\"true\"></i>Create Account</li>";
+                                  } else{
+                                      echo "<li class=\"resp-tab-item-profile\"><i class=\"fa fa-plane\" aria-hidden=\"true\"></i>Update Account</li>";
+                                  }
+                            ?>
 							<li class="resp-tab-item-profile"><i class="fa fa-ship" aria-hidden="true"></i>Change Password</li>
 							<li class="resp-tab-item-profile"><i class="fa fa-plane" aria-hidden="true"></i>Deactivate Account</li>
 						</ul>
@@ -139,21 +177,34 @@
 						<div class="resp-tabs-container">
 							<!-- Customer Profile section -->
 							<div class="tab-1 resp-tab-content">
-								<p class="secHead">Create Account</p>
-
+								<?php
+									 if(isset($profileresultset)) {
+										 echo "<p class=\"secHead\"> Update Account</p>";
+									 } else {
+									 	echo "<p class=\"secHead\"> Create Account</p>";
+									 }
+								?>
 								<div class="w3l-sign-in">
 									<form method="post" class="agile_form">
-										<input type="text" placeholder="First Name" name="fname" class="name agileits" required=""/>
-										<input type="text" placeholder="Last Name" name="lname" class="name agileits" required=""/>
-										<input type="text" placeholder="Phone Number" name="phone" class="name agileits" required=""/>
-										<input type="text" placeholder="Address : Street 1" name="street1" class="name agileits" required=""/>
-										<input type="text" placeholder="Address : Street 2" name="street2" class="name agileits" required=""/>
-										<input type="text" placeholder="City" name="city" class="name agileits" required=""/>
-										<input type="text" placeholder="State" name="state" class="name agileits" required=""/>
-										<input type="text" placeholder="Country" name="country" class="name agileits" required=""/>
-										<input type="text" placeholder="Zip" name="zip" class="name agileits" required=""/>
+										<input type="text" placeholder="First Name" name="fname" class="name agileits"
+				                              value="<?php echo !isset($profileresultset[0]) ? '' : $profileresultset[0]; ?>" required=""/>
+										<input type="text" placeholder="Last Name" name="lname" class="name agileits"
+                                              value="<?php echo !isset($profileresultset[1]) ? '' : $profileresultset[1]; ?>" required=""/>
+										<input type="text" placeholder="Phone Number" name="phone" class="name agileits"
+                                              value="<?php echo !isset($profileresultset[2]) ? '' : $profileresultset[2]; ?>" required=""/>
+										<input type="text" placeholder="Address : Street 1" name="street1" class="name agileits"
+                                              value="<?php echo !isset($profileresultset[3]) ? '' : $profileresultset[3]; ?>" required=""/>
+										<input type="text" placeholder="Address : Street 2" name="street2" class="name agileits"
+                                              value="<?php echo !isset($profileresultset[4]) ? '' : $profileresultset[4]; ?>" required=""/>
+										<input type="text" placeholder="City" name="city" class="name agileits"
+                                              value="<?php echo !isset($profileresultset[5]) ? '' : $profileresultset[5]; ?>" required=""/>
+										<input type="text" placeholder="State" name="state" class="name agileits"
+                                              value="<?php echo !isset($profileresultset[6]) ? '' : $profileresultset[6]; ?>" required=""/>
+										<input type="text" placeholder="Country" name="country" class="name agileits"
+                                              value="<?php echo !isset($profileresultset[7]) ? '' : $profileresultset[7]; ?>" required=""/>
+										<input type="text" placeholder="Zip" name="zip" class="name agileits"
+                                              value="<?php echo !isset($profileresultset[8]) ? '' : $profileresultset[8]; ?>" required=""/>
 										<p class="notPara"><br>Do you want offer notifications?&nbsp&nbsp&nbsp<input type="checkbox" name="notifyCheck" checked></p>
-
 										<div class="submit"><br>
 										  <input type="submit" value="Save">
 										  <input type="submit" value="Cancel">
@@ -161,39 +212,12 @@
 									</form>
 								</div>
 							</div>
-							<!-- Update Account section -->
-							<div class="tab-1 resp-tab-content">
-								<p class="secHead">Update Account Details</p>
-
-								<div class="w3l-sign-in">
-									<form action="#" method="post" class="agile_form">
-										<input type="text" placeholder="First Name" name="fname" class="name agileits" required=""/>
-										<input type="text" placeholder="Last Name" name="lname" class="name agileits" required=""/>
-										<input type="text" placeholder="Phone Number" name="phone" class="name agileits" required=""/>
-										<input type="text" placeholder="Address : Street 1" name="street1" class="name agileits" required=""/>
-										<input type="text" placeholder="Address : Street 2" name="street2" class="name agileits" required=""/>
-										<input type="text" placeholder="City" name="city" class="name agileits" required=""/>
-										<input type="text" placeholder="State" name="state" class="name agileits" required=""/>
-										<input type="text" placeholder="Country" name="country" class="name agileits" required=""/>
-										<input type="text" placeholder="Zip" name="zip" class="name agileits" required=""/>
-										<p class="notPara"><br>Do you want offer notifications?&nbsp&nbsp&nbsp<input type="checkbox" name="notifyCheck" checked></p>
-
-										<div class="submit"><br>
-										  <input type="submit" value="Update">
-										  <input type="submit" value="Cancel"><br><br>
-
-										</div>
-									</form>
-								</div>
-							</div>
-
-
 
 							<!--Change Password-->
 							<div class="tab-1 resp-tab-content">
 								<p class="secHead">Change Password</p>
 								<div class="agile-send-mail">
-									<form action="#" method="post" class="agile_form">
+									<form method="post" class="agile_form">
 										<input type="text" placeholder="Old Password" name="old-pwd" class="name agileits" required=""/>
 										<input type="text" placeholder="New Password" name="new-pwd" class="name agileits" required=""/>
 										<input type="text" placeholder="Confirm New Password" name="conf-new-pwd" class="name agileits" required=""/>
@@ -211,7 +235,7 @@
 								<p class="secHead">Deactivate Account</p><br>
 								<img class="settingImg" src="images/deactive.png" width="100" height="100">
 								<div class="agile-send-mail">
-									<form action="#" method="post" class="agile_form">
+									<form method="post" class="agile_form">
 										<p class="b_name" style="color: white;text-align: center;">Click on below button to Deactivate your account.</p><br>
 										<div class="submit"><br>
 										  <input type="submit" value="Deactivate">
